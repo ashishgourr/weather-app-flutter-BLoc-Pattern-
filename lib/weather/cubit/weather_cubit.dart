ccb2fc3982dc06da1d26627ae260e4d9ec2ed0fd
ashishg <ashishg@geekyants.com>
2021-12-28 18:41:27 +0530

    updated with bloc pattern

---
+++ lib/weather/cubit/weather_cubit.dart
@@ -0,0 +1,113 @@
+import 'package:equatable/equatable.dart';
+import 'package:flutter_weather_app/weather/models/weather.dart';
+import 'package:hydrated_bloc/hydrated_bloc.dart';
+import 'package:json_annotation/json_annotation.dart';
+import 'package:weather_repository/weather_repository.dart'
+    show WeatherRepository;
+
+part 'weather_cubit.g.dart';
+part 'weather_state.dart';
+
+class WeatherCubit extends HydratedCubit<WeatherState> {
+  WeatherCubit(this._weatherRepository) : super(WeatherState());
+
+  final WeatherRepository _weatherRepository;
+
+// fetchWeather(String? city) uses our weather repository to try and
+// retrieve a weather object for the given city
+
+  Future<void> fetchWeather(String? city) async {
+    if (city == null || city.isEmpty) return;
+// Emitting the loading state when user searches the city name.
+    emit(state.copyWith(status: WeatherStatus.loading));
+
+    try {
+      final weather = Weather.fromRepository(
+        await _weatherRepository.getWeather(city),
+      );
+      final units = state.temperatureUnits;
+      final value = units.isFahrenheit
+          ? weather.temperature.value.toFahrenheit()
+          : weather.temperature.value;
+
+      emit(
+        state.copyWith(
+          status: WeatherStatus.success,
+          temperatureUnits: units,
+          weather: weather.copyWith(temperature: Temperature(value: value)),
+        ),
+      );
+    } on Exception {
+      emit(state.copyWith(status: WeatherStatus.failure));
+    }
+  }
+
+  //refreshWeather() retrieves a new weather object using the
+  //weather repository given the current weather state
+
+  Future<void> refreshWeather() async {
+    if (!state.status.isSuccess) return;
+    if (state.weather == Weather.empty) return;
+    try {
+      final weather = Weather.fromRepository(
+        await _weatherRepository.getWeather(state.weather.location),
+      );
+      final units = state.temperatureUnits;
+      final value = units.isFahrenheit
+          ? weather.temperature.value.toFahrenheit()
+          : weather.temperature.value;
+
+      emit(
+        state.copyWith(
+          status: WeatherStatus.success,
+          temperatureUnits: units,
+          weather: weather.copyWith(temperature: Temperature(value: value)),
+        ),
+      );
+    } on Exception {
+      emit(state);
+    }
+  }
+
+  //toggleUnits() toggles the state between Celsius and Fahrenheit
+
+  void toggleUnits() {
+    final units = state.temperatureUnits.isFahrenheit
+        ? TemperatureUnits.celsius
+        : TemperatureUnits.fahrenheit;
+
+    if (!state.status.isSuccess) {
+      emit(state.copyWith(temperatureUnits: units));
+      return;
+    }
+
+    final weather = state.weather;
+    if (weather != Weather.empty) {
+      final temperature = weather.temperature;
+      final value = units.isCelsius
+          ? temperature.value.toCelsius()
+          : temperature.value.toFahrenheit();
+      emit(
+        state.copyWith(
+          temperatureUnits: units,
+          weather: weather.copyWith(temperature: Temperature(value: value)),
+        ),
+      );
+    }
+  }
+
+  @override
+  WeatherState fromJson(Map<String, dynamic> json) =>
+      WeatherState.fromJson(json);
+
+  @override
+  Map<String, dynamic> toJson(WeatherState state) => state.toJson();
+}
+
+//converting Fahrenheit (F) to Celsius (C)
+//C = 5/9 x (F-32)
+
+extension on double {
+  double toFahrenheit() => ((this * 9 / 5) + 32);
+  double toCelsius() => ((this - 32) * 5 / 9);
+}
